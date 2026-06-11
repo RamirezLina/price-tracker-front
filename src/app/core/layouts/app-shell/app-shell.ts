184748bfbbff36f188ef6fbf6d7177f3621ce886
Lina María <linaramirezm11@gmail.com>
2026-06-11 17:42:55 -0500

    [Add] Add basic components for user layout: header, footer, sidebar

--- src/app/core/layouts/app-shell/app-shell.ts
+++ src/app/core/layouts/app-shell/app-shell.ts
@@ -1,8 +1,11 @@
 import { Component, input } from '@angular/core';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+import { Sidebar } from "../sidebar/sidebar";
 
 @Component({
   selector: 'app-shell',
-  imports: [],
+  imports: [Header, Footer, Sidebar],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.css',
 })
