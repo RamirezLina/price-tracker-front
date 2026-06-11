3a9842c973be878c165457ac1457c6f5f0628268
Lina María <linaramirezm11@gmail.com>
2026-06-11 17:01:22 -0500

    [Add] Update project name and metadata for GlowPrice; add Tailwind CSS plugin and update Prettier configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,5 @@ import { AppShell } from "./core/layouts/app-shell/app-shell";
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('price-tracker-front');
+  protected readonly title = signal('GlowPrice | Comparador de precios dermocosméticos');
 }
