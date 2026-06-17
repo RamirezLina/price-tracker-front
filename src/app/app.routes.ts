d30183db80be7a1e3ac623c95cdd79ac9fdc15e5
Lina María <linaramirezm11@gmail.com>
2026-06-17 17:37:41 -0500

    [Update] Refactor app layout and routing: integrate new components and update sidebar links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ScrapProductPage } from './features/product-management/pages/scrap-product-page/scrap-product-page';
+import { ProductsLayout } from './features/products-price/components/products-layout/products-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductsLayout,
+    title: 'Rastreador de precios'
+  },
+  {
+    path: 'u/products-tracker',
+    component: ProductsLayout,
+    title: 'Rastreador de precios'
+  },
+  {
+    path: 'u/product-management',
+    component: ScrapProductPage,
+    title: 'Gestión de productos'
+  }
+
+];
