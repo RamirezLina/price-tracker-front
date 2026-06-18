1278c8b03008006f94abc2de84d1ee8e506166cf
Lina María <linaramirezm11@gmail.com>
2026-06-18 14:50:02 -0500

    [Fix] Ajusta el path para páginas anidadas en el layout de rastreador de precios

--- src/app/core/routing/app-routes.ts
+++ src/app/core/routing/app-routes.ts
@@ -6,14 +6,14 @@ export const APP_ROUTE_PATHS = {
   home: '',
   productsTracker: `${USER_PREFIX}/${PRODUCTS_TRACKER_PREFIX}`,
   productManagement: `${USER_PREFIX}/${PRODUCTS_MANAGEMENT_PREFIX}`,
-  productsTrackerToday: `${USER_PREFIX}/${PRODUCTS_TRACKER_PREFIX}/today`,
-  productsTrackerHistory: `${USER_PREFIX}/${PRODUCTS_TRACKER_PREFIX}/history`,
+  productsTrackerToday: `today`,
+  productsTrackerHistory: `history`,
 } as const;
 
 export const APP_ROUTE_LINKS = {
   home: '/',
   productsTracker: `/${APP_ROUTE_PATHS.productsTracker}`,
   productManagement: `/${APP_ROUTE_PATHS.productManagement}`,
-  productsTrackerToday: `/${APP_ROUTE_PATHS.productsTrackerToday}`,
-  productsTrackerHistory: `/${APP_ROUTE_PATHS.productsTrackerHistory}`,
+  productsTrackerToday: `/${APP_ROUTE_PATHS.productsTracker}/${APP_ROUTE_PATHS.productsTrackerToday}`,
+  productsTrackerHistory: `/${APP_ROUTE_PATHS.productsTracker}/${APP_ROUTE_PATHS.productsTrackerHistory}`,
 } as const;
